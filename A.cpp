7749c604fb0a19e5f515d6d6c779ebf2b2023b13
StiveMan1 <04024004zh@gmail.com>
2020-08-11 09:03:16 +0000

    A

--- A.cpp
+++ A.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Hello World!\n";
+  int  n;
+  cin>>n;
+  int a = n/5;
+  n-= a*5;
+  int b = n/2;
+  n-= b*2;
+  cout<< a<<"(5r) "<<b<<"(2r) "<<n<<"(1r)";
+  return 0;
 }
